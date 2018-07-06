5a46455969d5a60a89840a48413a6a995dbd02cc
Zhe <zhe@DESKTOP-I10QU6K.localdomain>
2018-07-06 14:53:20 -0400

    update makefile

--- test/ising_test.cpp
+++ test/ising_test.cpp
@@ -5,6 +5,13 @@ TEST(a,a){
   EXPECT_EQ(1.0,1.0);
 }
 
+TEST(ising_init, ising_init){
+  class Ising a(4,7);
+  EXPEXT_EQ(a.m, 4);
+  EXPECT_EQ(a.n, 7);
+  EXPECT_EQ(a.lattice[2][2], 0);
+  EXPECT_EQ(a.lattice[3][6], 0);
+}
 
 int main(int argc, char **argv){
   ::testing::InitGoogleTest(&argc, argv);
